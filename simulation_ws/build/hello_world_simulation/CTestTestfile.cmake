b993cb4066e88ba56815931cd10dc1a4ae6018d1
Dwrulesalot <36670310+Dwrulesalot@users.noreply.github.com>
2021-08-13 21:33:09 -0400

    temp

--- simulation_ws/build/hello_world_simulation/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/environment/aws-robomaker-sample-application-helloworld/simulation_ws/src/hello_world_simulation
-# Build directory: /home/ubuntu/environment/aws-robomaker-sample-application-helloworld/simulation_ws/build/hello_world_simulation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
